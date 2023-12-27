eb381a6b8d3c5fc563063635c10a7513008b7ef9
julien yassef <juyassef@gmail.com>
2023-12-27 08:46:34 +0100

    test bills: should navigate to NewBill page

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -5,9 +5,9 @@
 import { screen, waitFor, fireEvent } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
-import { ROUTES_PATH } from "../constants/routes.js";
+import { ROUTES_PATH, ROUTES } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-
+import Bills from "../containers/Bills.js";
 
 import router from "../app/Router.js";
 
@@ -36,67 +36,132 @@ describe("Given I am connected as an employee", () => {
     test("Then bills should be ordered from earliest to latest", () => {
       // Fonction de tri par ordre chronologique
       const chrono = (a, b) => new Date(a) - new Date(b);
-    
+
       // Création de la page (headlessDOM)
       document.body.innerHTML = BillsUI({ data: bills }); // Des dates potentiellement non triées
-    
+
       // Récupération des dates affichées sur la page
       const dates = screen
         .getAllByText(
           /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
         )
         .map((a) => a.innerHTML);
-    
+
       // Tri des dates dans l'ordre chronologique
       const datesSorted = [...dates].sort(chrono);
-    
+
       // Vérification que les dates affichées sont triées de la plus ancienne à la plus récente
       expect(dates).toEqual(datesSorted.reverse());
     });
 
-describe('handleClickNewBill', () => {
-  test('should navigate to NewBill page', () => {
-  
-   
-    // simulation un clic sur le bouton Newbill
-    fireEvent.click(document.querySelector('button[data-testid="btn-new-bill"]'));
+    describe("handleClickNewBill", () => {
+      test("should navigate to NewBill page", () => {
+        // Création de la page (headlessDOM)
+        document.body.innerHTML = BillsUI({ data: bills }); 
 
-    
+
+        // simule des donneés dans le localStorage
+        Object.defineProperty(window, localStorage, {
+          value: localStorageMock,
+        });
+        // simule un employé connecté
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            type: "Employee",
+          })
+        );
+
+        // we have to mock navigation to test it
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname });
+        };
+
+        // mock le store
+        const store = jest.fn();
+
+        // création de ma class qui gère la page
+        const billsContainer = new Bills({
+          document,
+          onNavigate,
+          store,
+          localStorage: localStorageMock,
+        });
+
+        // récupère le button new BIll
+        const buttonNewBill = screen.getByTestId("btn-new-bill");
+
+
+        // mock la fonction handleClickIconEy
+        const handleClickNewBill = jest.fn(() =>
+        billsContainer.handleClickNewBill()
+        );
+
+        // ajout un eventlistener au click sur l'icon
+        buttonNewBill.addEventListener("click", handleClickNewBill());
+
+        // simulation un clic sur le bouton Newbill
+        fireEvent.click(buttonNewBill)
+
+        expect(handleClickNewBill).toHaveBeenCalled();
+      
+      });
+    });
+
+    describe("handleClickIconEye", () => {
+      test("should update modal content and show the modal", () => {
+        // Création de la page (headlessDOM)
+        document.body.innerHTML = BillsUI({ data: bills }); 
+
+        // simule des donneés dans le localStorage
+        Object.defineProperty(window, localStorage, {
+          value: localStorageMock,
+        });
+        // simule un employé connecté
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            type: "Employee",
+          })
+        );
+
+        // we have to mock navigation to test it
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname });
+        };
+
+        // mock le store
+        const store = jest.fn();
+
+        // création de ma class qui gère la page
+        const billsContainer = new Bills({
+          document,
+          onNavigate,
+          store,
+          localStorage: localStorageMock,
+        });
+
+        $.fn.modal = jest.fn()
+
+        // ============================================
+
+        // récupère l'icon
+        const icon = screen.getAllByTestId("icon-eye")[0];
+
+        // mock la fonction handleClickIconEy
+        const handleClickIconEye = jest.fn(() =>
+          billsContainer.handleClickIconEye(icon)
+        );
+        // ajout un eventlistener au click sur l'icon
+        icon.addEventListener("click", handleClickIconEye(icon));
+
+        // on click sur l'icon
+        fireEvent.click(icon);
+
+        // on veut que la fonction soit appelé
+        expect(handleClickIconEye).toHaveBeenCalled();
+        expect($.fn.modal).toHaveBeenCalled()
+      });
+    });
   });
-});
-
-describe("handleClickIconEye", () => {
-  test("should update modal content and show the modal", () => {
-    // Ajouter l'icône et la modal au DOM
-    document.body.innerHTML = `
-      <div id="icon" data-bill-url="test-url.jpg"></div>
-      <div id="modaleFile"></div>
-    `;
-
-    // Sélectionner les éléments icon et modal
-    const icon = document.getElementById("eye");
-    const modal = document.getElementById("modaleFile");
-
-    // simulation un clic sur le bouton Newbill
-    fireEvent.click(document.querySelector('button[data-testid="icon-eye"]'));
-
-    // Appele la fonction handleClickIconEye
-    handleClickIconEye(icon);
-
-    // Vérifie que l'URL de la facture est récupérée
-    expect(icon.getAttribute("data-bill-url")).toBe("test-url.jpg");
-
-    // Vérifie que la largeur de l'img est correctement calculée
-    const expectedWidth = Math.floor(modal.clientWidth * 0.5);
-    expect(modal.img.width).toBe(expectedWidth);
-
-    // Vérifie que le contenu de la modal est mis à jour avec l'img
-    expect(screen.getByAltText("Bill").src).toBe("test-url.jpg");
-
-    // Vérifie que la modal est affichée
-    expect(modal.style.display).toBe("block");
-  });
-});
-
-});
 });
