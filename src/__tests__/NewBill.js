a59eec201eb0b83327e4275d29042b9e2e13066a
julien yassef <juyassef@gmail.com>
2024-01-03 16:54:22 +0100

    config test NewBill sur le constructeur

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,14 +5,40 @@
 import { screen, fireEvent } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import { ROUTES } from "../constants/routes.js";
+import store from "../__mocks__/store";
 
 
 describe("Given I am connected as an employee", () => {
+
+  // Configuration de localStorage
+  Object.defineProperty(window, "localStorage", {
+    value: localStorageMock,
+  });
+  window.localStorage.setItem(
+    "user",
+    JSON.stringify({
+      type: "Employee",
+    })
+  );
+
   describe("When I am on NewBill Page", () => {
+
+  // Configuration initiale avant chaque test pour une NewBill
+    const html = NewBillUI()
+    document.body.innerHTML = html
+    let newBill = new NewBill({
+      document,
+      onNavigate: (pathname) => document.body.innerHTML = ROUTES({ pathname }),
+      store: store,
+      localStorage: window.localStorage
+    })
+
     test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+   
+      
+
     })
   })
 })
