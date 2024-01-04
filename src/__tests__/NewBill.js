fb46305fd2df26245cf8ea00939d8c91800b556f
julien yassef <juyassef@gmail.com>
2024-01-04 12:11:38 +0100

    ajout test file valid et invalid

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,9 +2,10 @@
  * @jest-environment jsdom
  */
 
-import { screen, fireEvent } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+// Import des modules nécessaires
+import { screen, fireEvent } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import { ROUTES } from "../constants/routes.js";
 import store from "../__mocks__/store";
@@ -25,50 +26,100 @@ describe("Given I am connected as an employee", () => {
 
   describe("When I am on NewBill Page", () => {
 
-  // Configuration initiale avant chaque test pour une NewBill
-    const html = NewBillUI()
-    document.body.innerHTML = html
+    // Configuration initiale avant chaque test pour NewBill
+    const html = NewBillUI();
+    document.body.innerHTML = html;
     let newBill = new NewBill({
       document,
       onNavigate: (pathname) => document.body.innerHTML = ROUTES({ pathname }),
       store: store,
       localStorage: window.localStorage
-    })
+    });
 
-  describe("When I select a file", () => {
 
-    test("it should call handleChangeFile method", () => {
-      // Création d'un mock de handleChangeFile 
-      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+    describe("When I select a file", () => {
+      test("it should call handleChangeFile method", () => {
+        // Création d'un mock de handleChangeFile
+        const handleChangeFile = jest.fn(newBill.handleChangeFile);
 
-      // Récupération de l'élément d'entrée de fichier de l'interface utilisateur
-      const inputFile = screen.getByTestId('file');
+        // Récupération de l'entrée de fichier 
+        const inputFile = screen.getByTestId('file');
 
-      // Ajout d'un écouteur d'événements de changement sur l'élément d'entrée de fichier
-      inputFile.addEventListener('change', handleChangeFile);
+        // Ajout d'un écouteur d'événements de changement sur l'élément d'entrée de fichier
+        inputFile.addEventListener('change', handleChangeFile);
 
-      // Simulation d'un changement de fichier dans l'élément d'entrée de fichier
-      fireEvent.change(inputFile, {
-        target: {
-          files: [
-            new File(['testfile.png'], 'testfile.png', { type: 'image/png' }),
-            new File(['testfile.jpg'], 'testfile.jpg', { type: 'image/jpg' }),
-            new File(['testfile.jpeg'], 'testfile.jpeg', { type: 'image/jpeg' }),
-          ]
-        }
+        // Simulation d'un changement de fichier dans l'élément d'entrée de fichier
+        fireEvent.change(inputFile, {
+          target: {
+            files: [
+              new File(['testfile.png'], 'testfile.png', { type: 'image/png' }),
+              new File(['testfile.jpg'], 'testfile.jpg', { type: 'image/jpg' }),
+              new File(['testfile.jpeg'], 'testfile.jpeg', { type: 'image/jpeg' }),
+            ]
+          }
+        });
+
+        // Vérification si la méthode handleChangeFile a bien été appelée
+        expect(handleChangeFile).toHaveBeenCalled();
       });
 
-      // Vérification si la méthode handleChangeFile a bien été appelée
-    expect(handleChangeFile).toHaveBeenCalled();
-  });
-  
 
-    test("Then ...", () => {
-  
+      describe("and the file format is valid", () => {
+        test('it should update the input field', () => {
+
+          // Création d'un mock de handleChangeFile
+        const handleChangeFile = jest.fn(newBill.handleChangeFile);
+
+        // Récupération de l'entrée de fichier 
+        const inputFile = screen.getByTestId('file');
+
+        // Ajout d'un écouteur d'événements de changement sur l'élément d'entrée de fichier
+        inputFile.addEventListener('change', handleChangeFile);
+
+        // Simulation d'un changement de fichier dans l'élément d'entrée de fichier
+        fireEvent.change(inputFile, {
+          target: {
+            files: [
+              new File(['testfile.png'], 'testfile.png', { type: 'image/png' }),
+              new File(['testfile.jpg'], 'testfile.jpg', { type: 'image/jpg' }),
+              new File(['testfile.jpeg'], 'testfile.jpeg', { type: 'image/jpeg' }),
+            ]
+          }
+        });
+          // Vérification que le nom du fichier sélectionné est "testfile.png"
+          expect(inputFile.files[0].name).toBe("testfile.png");
+        });
+      });
+
       
+      describe("and the file format is not valid", () => {
+        test('it should not update the input field', () => {
+           // Création d'un mock de handleChangeFile
+        const handleChangeFile = jest.fn(newBill.handleChangeFile);
 
-    })
-  })
-  })
-})
+        // Récupération de l'entrée de fichier 
+        const inputFile = screen.getByTestId('file');
 
+        // Ajout d'un écouteur d'événements de changement sur l'élément d'entrée de fichier
+        inputFile.addEventListener('change', handleChangeFile);
+
+        // Simulation d'un changement de fichier dans l'élément d'entrée de fichier
+        fireEvent.change(inputFile, {
+          target: {
+              files: [
+                new File(['invalidTestFile.txt'], 'invalidTestFile.txt', { type: 'image/paint' }),
+              ]
+            }
+          })
+          // Vérification que handleChangeFile a renvoyé false
+          expect(handleChangeFile).toHaveReturnedWith(false)
+        });
+      });
+
+      
+      test("Then ...", () => {
+      
+      });
+    });
+  });
+});
