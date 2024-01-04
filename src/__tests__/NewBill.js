cd599f996ad6858cca42ef5aa9e216bd1f6b5342
julien yassef <juyassef@gmail.com>
2024-01-04 08:35:34 +0100

    test handlechangefile

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -35,12 +35,40 @@ describe("Given I am connected as an employee", () => {
       localStorage: window.localStorage
     })
 
+  describe("When I select a file", () => {
+
+    test("it should call handleChangeFile method", () => {
+      // Création d'un mock de handleChangeFile 
+      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+
+      // Récupération de l'élément d'entrée de fichier de l'interface utilisateur
+      const inputFile = screen.getByTestId('file');
+
+      // Ajout d'un écouteur d'événements de changement sur l'élément d'entrée de fichier
+      inputFile.addEventListener('change', handleChangeFile);
+
+      // Simulation d'un changement de fichier dans l'élément d'entrée de fichier
+      fireEvent.change(inputFile, {
+        target: {
+          files: [
+            new File(['testfile.png'], 'testfile.png', { type: 'image/png' }),
+            new File(['testfile.jpg'], 'testfile.jpg', { type: 'image/jpg' }),
+            new File(['testfile.jpeg'], 'testfile.jpeg', { type: 'image/jpeg' }),
+          ]
+        }
+      });
+
+      // Vérification si la méthode handleChangeFile a bien été appelée
+    expect(handleChangeFile).toHaveBeenCalled();
+  });
+  
+
     test("Then ...", () => {
-   
+  
       
 
     })
   })
+  })
 })
-
 
