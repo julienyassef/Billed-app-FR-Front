3b341750c0473529c25ec508a478c68b49d1f1c4
julien yassef <juyassef@gmail.com>
2024-01-08 17:32:57 +0100

    ajout ligne message error

--- src/views/DashboardFormUI.js
+++ src/views/DashboardFormUI.js
@@ -68,7 +68,7 @@ export default (bill) => {
         <div class="col-sm">
           <label for="file" class="bold-label">Justificatif</label>
             <div class='input-field input-flex file-flex'>
-            <span id="file-name-admin">${bill.fileName}</span>
+            ${bill.fileName ? `<span id="file-name-admin">${bill.fileName}</span>` : `<span id="file-name-admin">Erreur: Format de fichier non pris en charge</span>`}
             <div class='icons-container'>
               <span id="icon-eye-d" data-testid="icon-eye-d" data-bill-url="${bill.fileUrl}"> ${eyeWhite} </span>
             </div>
